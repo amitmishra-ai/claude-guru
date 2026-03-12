81c64b9f09d45b16d0321907b45b34eeff8bf18e
sachinsinha0 <sachin.sinha@greatlearning.in>
2026-03-12 19:31:14 +0530

    Logo added

---
+++ dist/sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = 'guru-dashboard-v1';
+
+// Install: cache the app shell
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) =>
+      cache.addAll([
+        '/',
+        '/index.html',
+        '/favicon.png',
+        '/icons/icon-192.png',
+        '/icons/icon-512.png',
+      ])
+    )
+  );
+  self.skipWaiting();
+});
+
+// Activate: clean up old caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+// Fetch: network-first strategy
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const clone = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
